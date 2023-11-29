5d4e73d777d04bb430c71fcfaf8f34c4d6c4c995
Максим <Maxim>
2023-11-29 18:43:46 +0200

    added one more println in Test.java ta the feature branch

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] agrs){
         System.out.println("education");
         //comment on second string of the func
+        System.out.println(&quot;DICT&quot;);
     }
 }
