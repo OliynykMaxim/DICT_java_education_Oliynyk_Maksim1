d0ff65dd37d67bce91477e12c86f56dc118fe5cb
Максим <Maxim>
2023-11-30 14:55:35 +0200

    Hangman 4-st stage

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -10,8 +10,10 @@ public class Hangman {
         int randomIndex = random.nextInt(wordList.length);
         String secretWord = wordList[randomIndex];
 
+        String hint = getHint(secretWord); // Отримуємо підказку
+
         System.out.println("HANGMAN");
-        System.out.print("Guess the word: > ");
+        System.out.print("Guess the word " + hint + ": > ");
 
         Scanner scanner = new Scanner(System.in);
         String guessedWord = scanner.nextLine().toLowerCase();
@@ -22,6 +24,22 @@ public class Hangman {
             System.out.println("You lost!");
         }
     }
+
+    // Метод для отримання підказки
+    private static String getHint(String word) {
+        StringBuilder hint = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (i < 2) {
+                hint.append(word.charAt(i));
+            } else {
+                hint.append("-");
+            }
+        }
+        return hint.toString();
+    }
 }
+
+
+
 
 
