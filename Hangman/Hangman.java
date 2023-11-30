97d0be8cf2998bd81774e78625c5507e63f761b0
Максим <Maxim>
2023-11-30 14:52:19 +0200

    Hangman 2-st stage

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -1,2 +1,23 @@
-package Hangman;public class Hangman {
+package Hangman;
+
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.print("Guess the word: > ");
+
+        Scanner scanner = new Scanner(System.in);
+        String guessedWord = scanner.nextLine().toLowerCase(); // припустимо, слово заздалегідь встановлено і переведено у нижній регістр
+
+        // Припустимо, що слово для вгадування - "java"
+        String secretWord = "java";
+
+        if (guessedWord.equals(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
 }
+
